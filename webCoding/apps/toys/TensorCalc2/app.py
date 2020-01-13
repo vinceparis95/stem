64b992ca898518607c45c8904aa321bbccbc524c
vince <vinceparis19@gmail.com>
2020-01-13 16:42:44 -0500

    added webcoding app toy tensorcalc2 files

--- webCoding/apps/toys/TensorCalc2/app.py
+++ webCoding/apps/toys/TensorCalc2/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import numpy as np
-import matrixMath.quantumComputing.bits.matrices.unitaries
+from matrixMath.quantumComputing.bits.matrices.unitaries import isUnitary
 
 app = Flask(__name__)
 
@@ -20,21 +20,22 @@ def send():
 
     if request.method == 'POST':
         num1 = request.form['num1']
-        num2 = request.form['num2']
         # separate and convert numbers to floats
         num1 = [float(i) for i in num1.split()]
-        num2 = [float(i) for i in num2.split()]
         array1 = np.array([num1])
         print("array 1 (row, or bra) = \n", array1, "\n")
-        array2 = np.array([num2])
         # transpose second array
-        array2 = array2.transpose()
+        array2 = array1.transpose()
         print("array 2 (column, or ket) = \n", array2, "\n")
         operation = request.form['operation']
         if operation == 'unitarity':
+            unitary = isUnitary(array1)
+            print(unitary)
+            return render_template('home.html', sum=unitary)
 
 
 
 ######################################
+
 if __name__ == "__main__":
     app.run(debug=True)
