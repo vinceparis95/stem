af8e30c8ea8ea16ade99550e8d8f8a9191ffbee4
vince <vinceparis19@gmail.com>
2020-01-13 22:11:49 -0500

    refined webcoding app toy tensorcalc2

--- webCoding/apps/toys/TensorCalc2/app.py
+++ webCoding/apps/toys/TensorCalc2/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 from matrixMath.quantumComputing.bits.matrices.unitaries import isUnitary
-
+from matrixMath.quantumComputing.bits.matrices.eigens import eigens2
 app = Flask(__name__)
 
 #######################################
@@ -14,25 +14,25 @@ def home():
 
 ########################################
 
-@app.route("/send", methods=['POST'])
+@app.route("/calc", methods=['POST'])
 # define send message
-def send():
+def calc():
 
     if request.method == 'POST':
         num1 = request.form['num1']
         # separate and convert numbers to floats
         num1 = [float(i) for i in num1.split()]
         array1 = np.array([num1])
-        print("array 1 (row, or bra) = \n", array1, "\n")
-        # transpose second array
-        array2 = array1.transpose()
-        print("array 2 (column, or ket) = \n", array2, "\n")
+        array1 = array1.reshape(2, 2)
         operation = request.form['operation']
         if operation == 'unitarity':
             unitary = isUnitary(array1)
             print(unitary)
             return render_template('home.html', sum=unitary)
-
+        elif operation == 'eigenvalue':
+            eigen = eigens2(array1)
+            print(eigen)
+            return render_template('home.html', sum=eigen)
 
 
 ######################################
