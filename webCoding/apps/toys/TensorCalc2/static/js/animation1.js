50dcbee6f473d1957113b7f2d1563bdb64f5eb5f
vince <vinceparis19@gmail.com>
2020-01-14 16:49:44 -0500

    added tensorcalc toy app matrix sliding animation

--- webCoding/apps/toys/TensorCalc2/static/js/animation1.js
+++ webCoding/apps/toys/TensorCalc2/static/js/animation1.js
@@ -1,58 +1,95 @@
+//
+// function myMove() {
+//
+//     const elem = document.getElementById("myAnimation");
+//     let pos = 0;
+//     let R = 0;
+//     let G = 0;
+//     let B = 60;
+//     setInterval(frame, 5);
+//
+//     function frame() {
+//
+//         if (pos < 180) {
+//             pos++;
+//             elem.style.top = pos + 'px';
+//             let rgbColor = "rgb(" + R + ", " + G + ", " + B + ")";
+//             elem.style.backgroundColor = rgbColor;
+//             R = R + 1;
+//             G = G + 2;
+//             B = B + 1;
+//         }
+//     }
+// }
+
+//
+// function myMove2() {
+//
+//     const elem = document.getElementById("myAnimation");
+//     let pos = 0;
+//     let R = 0;
+//     let G = 0;
+//     let B = 60;
+//     setInterval(frame, 5);
+//
+//     function frame() {
+//
+//         if (pos < 180) {
+//             pos++;
+//             elem.style.top = pos + 'px';
+//             let rgbColor = "rgb(" + R + ", " + G + ", " + B + ")";
+//             elem.style.backgroundColor = rgbColor;
+//             R = R + 1;
+//             G = G + 2;
+//             B = B + 1;
+//         }
+//     }
+// }
+
+
 
 function myMove() {
-
     const elem = document.getElementById("myAnimation");
     let pos = 0;
-    let id = setInterval(frame, 5);
+    let R = 90;
+    let G = 95;
+    let B = 190;
+    let pos2 = 0
 
-
-    let R = 0;
-    let G = 0;
-    let B = 90;
-
-    full = 0;
-
+    setInterval(frame, 10);
 
     function frame() {
-    // if (pos == 180) {
-    //     elem.style.top = 0 + 'px';
-    //     elem.style.right = 0;
-    //     clearInterval(id);
-    //     }
-    // else {
-    //     pos++;
-    //     elem.style.top = pos + 'px';
-    //     let rgbColor = "rgb(" + R + ", " + G + ", " + B + ")";
-    //     elem.style.backgroundColor = rgbColor;
-    //     R = R + 1;
-    //     G = G + 2;
-    //     B = B + 1;
-    //     }
-
-        if (pos <181) {
+        if (pos < 180) {
             pos++;
             elem.style.top = pos + 'px';
-            let rgbColor = "rgb(" + R + ", " + G + ", " + B + ")";
-            elem.style.backgroundColor = rgbColor;
+            elem.style.backgroundColor = "rgb(" + R + ", " + G + ", " + B + ")";
             R = R + 1;
             G = G + 2;
             B = B + 1;
-        }
-        else if (pos >=181 && pos < 360) {
-            elem.style.right= 0;
-            pos++;
-            elem.style.top = 0 + 'px';
-            let rgbColor = "rgb(" + R + ", " + G + ", " + B + ")";
-            elem.style.backgroundColor = rgbColor;
+        } else if (pos == 180) {
+            elem.style.right = 0;
+            elem.style.top = 0;
+            pos++
+        } else if (pos > 180 && pos2 < 180) {
+            pos2++;
+            elem.style.top = pos2 + 'px';
+            elem.style.backgroundColor = "rgb(" + R + ", " + G + ", " + B + ")";
             R = R + 1;
             G = G + 2;
             B = B + 1;
-        }
-        else if (pos == 360){
 
-            clearInterval(id);
         }
 
+    }
 
-  }
 }
+
+
+
+
+function newFunction() {
+    myMove();
+
+}
+
+
