ee54894320dd0205a13a849da364777aef4dbb4d
vince <vinceparis19@gmail.com>
2020-01-28 12:42:13 -0500

    added quantum computing circuit  bit for basic circuits and basic machine learning bit for simple quantum neural network

---
+++ matrixMath/quantumComputing/bits/basicQNN/qnn.py
@@ -0,0 +1,13 @@
+import numpy as numpy
+import cirq as cq
+
+
+########################################################
+
+
+########################################################
+
+
+# a zgate class with variable weight
+# class zgate(cq.ops.eigen_gate.EigenGate,
+#             cq.ops.gate_features.TwoQubitGate):
---
+++ matrixMath/quantumComputing/bits/circuits/basic.py
@@ -0,0 +1,30 @@
+import numpy as np
+from matrixMath.quantumComputing.bits.matrices.unitaries import isUnitary
+#################################################
+
+
+
+#################################################
+
+# a cnot on a three qubit system
+
+matrix1 = np.array([[0, 1, 0, 0, 0, 0, 0, 0],
+                    [1, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 1, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 1],])
+
+
+
+isUnitary(matrix1)
+
+
+oneonezero = np.array([[1, 0, 0, 0, 0, 0, 0, 0]]).transpose()
+
+print(oneonezero)
+
+print(np.dot(matrix1,oneonezero))
+
