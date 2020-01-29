5373ad8faea57f5d2a7e5193efea305ec1e8604b
vince <vinceparis19@gmail.com>
2020-01-29 15:03:37 -0500

    added adinkra animation for boson/fermion graph

--- webCoding/interfaces/fruits/matrixLab/static/js/adinkras/adinkra.js
+++ webCoding/interfaces/fruits/matrixLab/static/js/adinkras/adinkra.js
@@ -1,2 +1,37 @@
-const element = <h1>Hello, world</h1>;
-ReactDOM.render(element, document.getElementById('jawn'));
+
+
+function AdinkraA() {
+  return <div>
+      <h2>adinkra</h2>
+      <div className={"container4"}>
+          <div className={"vertices"}>
+          </div>
+
+      </div>
+  </div>
+}
+
+function AdinkraB() {
+
+  return <div>
+      <h2>adinkra</h2>
+      <div className={"container4"}>
+      <div className={"animation-target"}>
+</div>
+
+      </div>
+  </div>
+}
+
+function Select(props) {
+  const select = props.select;
+  if (select) {
+    return <AdinkraA />;
+  }
+  return <AdinkraB />;
+}
+
+ReactDOM.render(
+  <Select select={false} />,
+  document.getElementById('root')
+);
--- webCoding/interfaces/fruits/matrixLab/static/js/matrixSliders/matrixSliders.js
+++ webCoding/interfaces/fruits/matrixLab/static/js/matrixSliders/matrixSliders.js
@@ -10,7 +10,7 @@ function myMove() {
     function frame() {
         if (pos3 < 180) {
             pos3++;
-            elem.style.left = pos3 + 'px';
+            elem.style.top = pos3 + 'px';
             elem.style.backgroundColor = "rgb(" + R + ", " + G + ", " + B + ")";
             R = R + 1; G = G + 2; B = B + 1;
         }
@@ -19,25 +19,25 @@ function myMove() {
         }
         else if (pos3 > 180 && pos4 < 180) {
             pos4++;
-            elem.style.left = pos4 + 'px';
+            elem.style.top = pos4 + 'px';
             elem.style.backgroundColor = "rgb(" + R + ", " + G + ", " + B + ")";
             R = R + 1; G = G + 2; B = B + 1;
         }
         else if (pos4 == 180) {
             elem.style.bottom = 0;
-            elem.style.left = 0;
+            elem.style.left = 180 + 'px';
             pos4++
         }
          else if (pos4 > 180 && pos7 < 180) {
             pos7++;
 
-            elem.style.left = pos7 + 'px';
+            elem.style.top = pos7 + 'px';
             elem.style.backgroundColor = "rgb(" + R + ", " + G + ", " + B + ")";
             R = R + 1; G = G + 2; B = B + 1;
         }
          else if (pos7 <= 180 && pos8 < 180){
              pos8++;
-             elem.style.left = pos8 + 'px';
+             elem.style.top = pos8 + 'px';
              elem.style.backgroundColor = "rgb(" + R + ", " + G + ", " + B + ")";
              R = R + 1; G = G + 2; B = B + 1;
         }
@@ -55,31 +55,31 @@ function myMove2() {
     function frame() {
         if (pos < 180) {
             pos++;
-            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
             elem.style.backgroundColor = "rgb(" + R + ", " + G + ", " + B + ")";
             R = R + 1; G = G + 2; B = B + 1;
         } else if (pos == 180) {
-            elem.style.right = 0;
+            elem.style.top = 180+'px';
             pos++
         } else if (pos > 180 && pos2 < 180) {
             pos2++;
-            elem.style.top = pos2 + 'px';
+            elem.style.left = pos2 + 'px';
             elem.style.backgroundColor = "rgb(" + R + ", " + G + ", " + B + ")";
             R = R + 1; G = G + 2; B = B + 1;
         }
         else if (pos2 == 180) {
             pos2++
+            elem.style.top = 0 + 'px'
         } else if (pos2 > 180 && pos5 < 180) {
             pos5++;
-            elem.style.left = '0px';
-            elem.style.top = pos5 + 'px';
+            elem.style.left = pos5+'px';
             elem.style.backgroundColor = "rgb(" + R + ", " + G + ", " + B + ")";
             R = R + 1; G = G + 2; B = B + 1;
         }
         else if (pos5 >= 180 && pos6 < 180) {
             pos6++;
-            elem.style.left = '180px';
-            elem.style.top = pos6 + 'px';
+            elem.style.top = '180px';
+            elem.style.left = pos6 + 'px';
             elem.style.backgroundColor = "rgb(" + R + ", " + G + ", " + B + ")";
             R = R + 1; G = G + 2; B = B + 1;
         }
