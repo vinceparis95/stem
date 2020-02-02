1b1b19c7519d59c99d843305da75c9ca1a462a6b
vince <vinceparis19@gmail.com>
2020-02-02 02:58:23 -0500

    restructured matrix byte in la block

--- matrixMath/La/bytes/matrix/tensor.py
+++ matrixMath/La/bytes/matrix/tensor.py
@@ -6,7 +6,7 @@ import numpy as np
 # a scalar
 
 scalar = np.array([1])
-print("scalar: \n", scalar, "\n")
+print(scalar.shape)
 
 
 #####################################
@@ -14,8 +14,7 @@ print("scalar: \n", scalar, "\n")
 # a vector
 
 vector = np.array([1, 2, 3])
-print("vector: ", vector)
-print("shape: \n", vector.shape, "\n")
+print(vector.shape)
 
 
 ######################################
@@ -25,13 +24,12 @@ print("shape: \n", vector.shape, "\n")
 matrix = np.array([[1, 2, 3],
                    [1, 2, 3],
                    [1, 2, 3]])
-print("2-D matrix: \n", matrix)
-print("shape: \n", matrix.shape, "\n")
+print(matrix.shape)
 
 
 #######################################
 
-# a 3-D tensor
+# a 3-D matrix
 
 tensor = np.array([[[1, 2, 3],
                     [1, 2, 3],
@@ -39,8 +37,7 @@ tensor = np.array([[[1, 2, 3],
                    [[1, 0, 0],
                     [0, 1, 0],
                     [0, 0, 1]]])
-print("3-D matrix: \n", tensor)
-print("shape: \n", tensor.shape, "\n")
+print(tensor.shape)
 
 
 ########################################
