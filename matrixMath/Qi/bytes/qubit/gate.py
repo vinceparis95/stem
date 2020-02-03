21fecae8f2428e31415adbba71d22915f0a4256d
vince <vinceparis19@gmail.com>
2020-02-02 20:31:20 -0500

    refined gate and qubit bits in qubit byte

---
+++ matrixMath/Qi/bytes/qubit/gate.py
@@ -0,0 +1,59 @@
+###########################################################
+
+from matrixMath.Qi.bytes.qubit.qubitTest import isUnitary
+import numpy as np
+import math
+
+####################################
+
+# t h e  G a t e s
+
+#################################
+
+# the X Matrix
+x = np.array([[0, 1], [1, 0]])
+# a flippable one state
+state1 = [1,0]
+flipped = np.dot(x,state1)
+print(flipped)
+
+###################################
+
+
+# The Z Gate
+
+z = np.array([[1, 0], [0, -1]])
+
+
+################################
+
+
+
+##############################
+
+# # Dirac Hadamard
+# #       | 0 } + | 1 }
+# #       _____________
+# #           root2
+#
+# # matrix   [[1, 1],
+# #          [ 1,-1]])
+#
+#
+# # create the basic matrix
+# h = np.array([[1, 1],
+#               [1, -1]])
+# # get the matrix transpose
+# htranspose = h.transpose()
+# # get the haddy coefficient
+# hcoefficient = 1/(math.sqrt(2))
+# # dot the matrix with its transpose
+# dot = np.dot(h,htranspose)
+# # square the coefficient
+# hcoefficientmul = hcoefficient*hcoefficient
+# # print the squared coefficient with the dot
+# print(hcoefficientmul*dot)
+
+
+#############################################
+
