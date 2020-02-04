a412d763d0972558c4486fefdc76196474e50cdd
vince <vinceparis19@gmail.com>
2020-02-04 01:42:07 -0500

    refined qubity bit in qubit byte

--- matrixMath/qi/bytes/qubit/qubity.py
+++ matrixMath/qi/bytes/qubit/qubity.py
@@ -1,66 +1,62 @@
-
-
 import numpy as np
-from matrixMath.qi.fruits.matrixGen.MatrixGenerator import matrix
 
 
-####################################
+#############################
 
-# E i g e n v a l u e
+# U n i t a r i t y
 
-####################################
+############################
 
 # create the matrix
 
-x = np.array([[1, 0], [0, 1]])
+x = np.array([[0, 1],
+              [1, 0]])
+
+############################
 
 
-######################################
+def isUnitary(m):
+    tmatrix = m.transpose()
+    i = np.matmul(m, tmatrix)
+    return i
 
-# create the lamda
 
-# set lambda to 1
-l = 2
-# n x n I matrix * scalar λ (lambda)
+print(isUnitary(x))
+
+
+############################
+
+# the E i g e n v a l u e
+
+############################
+
+# create the lamda (λ)
+
+# set λ to 1
+l = 1
+# i matrix * λ
 i = np.eye(2)
-# dot lambda with i matrix for lam diagonal
+# dot λ with i
 lam = (np.dot(i, l))
-# subtraction of I multiple from A matrix
-x = x - lam
+# subtract λi from  matrix
+z = x - lam
+print(z)
 
-#######################################
+############################
 
-# find if the determinant is eigenvalue
+# find the determinant
+
 # det = det(A-λI)=0
-
-def isEigen(matrix):
-    det = matrix
+def isEigen(input):
+    det = input
     det2 = []
     for x in det:
         for y in x:
             det2.append(y)
-    det3 = (det2[0] * det2[3]) - (det2[1] * det2[2])
-    if det3 == 0:
-        return det3
-    elif det3 != 0:
-        return det3
-
-# simple aeigenvalue getter
-def eigens2(x):
-    return np.linalg.eigvals(np.array(matrix))
-
-isEigen(x)
-
-#####################################################
-
-# U n i t a r i t y
-
-#####################################################
+    det3 = (det2[0] * det2[3]) \
+         - (det2[1] * det2[2])
+    if det3==0:
+        print(det3)
 
 
-def isUnitary(m):
-      tmatrix = m.transpose()
-      i = np.matmul(m, tmatrix)
-      print("if i matrix, then unitary: ")
-      return i
-
+isEigen(z)
