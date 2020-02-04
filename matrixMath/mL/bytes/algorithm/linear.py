cedeee9abdd23422e9b263eb1952d58829e18d66
vince <vinceparis19@gmail.com>
2020-02-03 21:53:22 -0500

    added linear bit in algorithm byte

---
+++ matrixMath/mL/bytes/algorithm/linear.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import pathlib
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+t = tf.Variable
+
+print(t)
