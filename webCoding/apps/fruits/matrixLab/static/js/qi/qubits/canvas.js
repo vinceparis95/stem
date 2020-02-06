502d40d3f6bf4ef25c4856b99303d5815f13a750
vince <vinceparis19@gmail.com>
2020-02-06 02:31:45 -0500

    restructured matrixLab in app fruit

---
+++ webCoding/apps/fruits/matrixLab/static/js/qi/qubits/canvas.js
@@ -0,0 +1,17 @@
+
+function Score() {
+  return <div>
+
+      <div className={"container5"}>
+
+      </div>
+  </div>
+}
+
+
+
+ReactDOM.render(
+  <Score />,
+  document.getElementById('root2')
+);
+
---
+++ webCoding/apps/fruits/matrixLab/static/js/qi/qubits/qubit.js
@@ -0,0 +1,55 @@
+
+
+
+
+var state = anime.timeline({
+    loop:true,
+    duration:44
+})
+
+
+
+function entangle() {
+    state
+        .add({
+            targets: '.container54',
+            keyframes: [
+                {translateX: -19},
+                {translateX:  19},
+            ],
+            background: 'rgb(255,0,199)',
+            direction: 'alternate',
+            easing: 'linear',
+            autoplay: false
+        })
+        .add({
+            targets: '.container54',
+            keyframes: [
+                {translateX: -19},
+                {translateX:  19},
+            ],
+            background: 'rgb(13,255,2)',
+            direction: 'alternate',
+            easing: 'linear',
+            autoplay: false
+        })
+        .add({
+            targets: '.container54',
+            keyframes: [
+                {translateX: -19},
+                {translateX:  19},
+            ],
+            background: 'rgb(255,0,199)',
+            direction: 'alternate',
+            easing: 'linear',
+            autoplay: false
+        })
+
+    return (
+  <div>Allah'u'Abha
+  </div>
+);
+
+}
+
+
