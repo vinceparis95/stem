53cc9f06d52685add793053f8ef46cb8ed939a14
vince <vinceparis19@gmail.com>
2020-02-08 02:31:53 -0500

    refined qubit cell in matrixLab fruit (reanimated entangled qubit)

--- webCoding/apps/fruits/matrixLab/static/js/qi/qubits/qubit.js
+++ webCoding/apps/fruits/matrixLab/static/js/qi/qubits/qubit.js
@@ -5,7 +5,7 @@
 //
 var state = anime.timeline({
     loop:true,
-    duration: 36
+    duration: 12
 })
 
 
@@ -18,7 +18,7 @@ function entangle() {
                 {translateX: -14},
 
             ],
-            background: 'rgb(13,255,2)',
+            background: 'rgb(255,0,199)',
             direction: 'alternate',
             easing: 'linear',
             autoplay: false
@@ -36,23 +36,14 @@ function entangle() {
         .add({
             targets: '.container54',
             keyframes: [
-                {translateX: -14},
+                {translateX: 7},
             ],
             background: 'rgb(13,255,2)',
             direction: 'alternate',
             easing: 'linear',
             autoplay: false
         })
-        .add({
-            targets: '.container54',
-            keyframes: [
-                {translateX: 14},
-            ],
-            background: 'rgb(255,0,199)',
-            direction: 'alternate',
-            easing: 'linear',
-            autoplay: false
-        })
+
 
     return (
         <div>Allah'u'Abha
