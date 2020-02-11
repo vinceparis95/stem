3f9e1ad90131bf45060e7bacca1632bc02281b5b
vince <vinceparis19@gmail.com>
2020-02-11 02:56:24 -0500

    restructured webcoding path into arch, bots, and ops blocks

--- webCoding/arch/fruits/αrcαdϵ/app.py
+++ webCoding/arch/fruits/αrcαdϵ/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #######################################
 
 
-# define the architecture
+# define the arch
 @app.route("/")
 def home():
     return render_template("home.html")
