ddb27eb9f1e6b499c9cf9c17efce10378248fa68
vince <vinceparis19@gmail.com>
2020-02-14 13:26:39 -0500

    refined bucket/arcade

--- bucket/arcade/app.py
+++ bucket/arcade/app.py
@@ -5,48 +5,56 @@ from sciCode.qi.qubit.qubity import isEigen
 app = Flask(__name__)
 
 #######################################
-
-
-# define the uiux
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template('portal.html')
 
-########################################
-
-#calc
-
-@app.route("/calc", methods=['POST'])
-# define send message
-def calc():
-
-    if request.method == 'POST':
-        num1 = request.form['num1']
-        # separate and convert numbers to floats
-        num1 = [float(i) for i in num1.split()]
-        array1 = np.array([num1])
-        array1 = array1.reshape(2, 2)
-        operation = request.form['operation']
-        if operation == 'unitarity':
-            unitary = isUnitary(array1)
-            print(unitary)
-            return render_template('home.html', sum=unitary)
-        elif operation == 'eigenvalue':
-            eigen = isEigen(array1)
-            print(eigen)
-            return render_template('home.html', sum=eigen)
 
 
 ###################################################
+
 
 @app.route("/qi")
 # test channel
 def qi():
     return render_template('qi.html')
 
+###################################################
 
+
+@app.route('/ml')
+def mL():
+    return render_template('ml.html')
 
 ######################################
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+######################################################
+
+
+# define the uiux
+
+# @app.route("/", methods=['POST'])
+# # define send message
+# def calc():
+#
+#     if request.method == 'POST':
+#         num1 = request.form['num1']
+#         # separate and convert numbers to floats
+#         num1 = [float(i) for i in num1.split()]
+#         array1 = np.array([num1])
+#         array1 = array1.reshape(2, 2)
+#         operation = request.form['operation']
+#         if operation == 'unitarity':
+#             unitary = isUnitary(array1)
+#             print(unitary)
+#             return render_template('ml.html', sum=unitary)
+#         elif operation == 'eigenvalue':
+#             eigen = isEigen(array1)
+#             print(eigen)
+#             return render_template('ml.html', sum=eigen)
+#
