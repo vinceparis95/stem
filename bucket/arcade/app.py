5fb92055e36e644d08ced026b113cf549c5c9bcc
vince <vinceparis19@gmail.com>
2020-02-14 16:45:18 -0500

    refined sciCode routes in arcade app

--- bucket/arcade/app.py
+++ bucket/arcade/app.py
@@ -1,42 +1,51 @@
 from flask import Flask, render_template, request
-import numpy as np
-from sciCode.qi.qubit.qubity import isUnitary
-from sciCode.qi.qubit.qubity import isEigen
+
+
+#####################
+
+
 app = Flask(__name__)
 
-#######################################
-@app.route("/")
-def home():
-    return render_template('portal.html')
+
+############################################
 
 
+# App routes for SciCode path
 
-###################################################
+
+@app.route('/la')
+def la():
+    return render_template('sciCode/la.html')
+
+
+@app.route('/ml')
+def mL():
+    return render_template('sciCode/ml.html')
 
 
 @app.route("/qi")
 # test channel
 def qi():
-    return render_template('qi.html')
-
-###################################################
+    return render_template('sciCode/qi.html')
 
 
-@app.route('/ml')
-def mL():
-    return render_template('ml.html')
+#############################################
 
-######################################
+
 
 if __name__ == "__main__":
+
     app.run(debug=True)
 
 
+###########################
 
-######################################################
 
+import numpy as np
+from sciCode.qi.qubit.qubity import isUnitary
+from sciCode.qi.qubit.qubity import isEigen
 
-# define the uiux
+# define the app
 
 # @app.route("/", methods=['POST'])
 # # define send message
