f557e86902ae6dedb63e48cef2092df5b8d19606
Vincent Paris <vincentparis@Vincents-MBP.fios-router.home>
2020-03-15 03:19:38 -0400

    refined nnet (tanna0ad) in webcode/cell

--- webCode/cell/tanna0ad/App.js
+++ webCode/cell/tanna0ad/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View , TouchableOpacity} from 'react-native';
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={{ backgroundColor:'#e9ffc9',flex: 1,
+      <View style={{ backgroundColor:'#e9ffc945',flex: 1,
        justifyContent: "center", alignItems: "center" }}>
-        <Text style={{fontFamily: 'Courier' ,fontSize:26}}>Allah'u'Abha :)</Text>
+        <Text style={{fontSize:26, color: "#85321b45",
+        fontFamily: "Courier"}}> neighborhood net </Text>
+        <TouchableOpacity
+          style={{
+            backgroundColor:'#a4a6fc19',
+            padding: 144,
+            borderRadius: 19,
+
+          }}
+        >
+         <Text
+           style={{
+             color: "white",
+             fontSize: 20
+           }}
+         >
+           </Text>
+         </TouchableOpacity>
       </View>
     );
   }
