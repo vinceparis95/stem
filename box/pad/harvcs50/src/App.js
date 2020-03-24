8b3f3613a78e0b5412b3a9b94146f3bfac9bd208
Vincent Paris <vincentparis@Vincents-MBP.fios-router.home>
2020-03-24 14:20:11 -0400

    added harvard course react project to box pad

---
+++ box/pad/harvcs50/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{height:'45px', width:'45px', backgroundColor:'blue'}}>
+    <div ></div>
+    </div>
+  );
+}
+
+export default App;
--- box/pad/job1.js
+++ box/pad/job1.js
@@ -1,6 +1,9 @@
-":)"
 
+/////////////////////////////
 
+// // Functions
+//
+//
 // function a(){
 //     console.log(":) <3")
 // }
@@ -15,20 +18,77 @@
 //
 // c()
 
-arr = [1,2,3,4]
+////////////////////////////////////////////////
 
-class Set {
-  constructor(arr) {
-    this.arr = arr
-  }
+//// Classes, constructors, methods
 
-  add(val) {
-    this.arr.push(val)
-  }
+// arr = [1,2,3,4]
 
-}
+// class Set {
+//   constructor(arr) {
+//     this.arr = arr
+//   }
+//
+//   add(val) {
+//     if (!this.has(val)) this.arr.push(val)
+//   }
+//
+//   delete(val){
+//     this.arr = this.arr.filter(x =>  x !== val)
+//   }
+//
+//   has(val){
+//     return this.arr.includes(val)
+//   }
+//
+//
+// }
+//
+// const s = new Set([1,2,3,4])
+// s.add(9)
+// s.add(101)
+// s.add(12)
+//
+// for (i=0;i<=arr.length; i++){
+//   console.log(s.arr[i])
+//   console.log(":)")
+// }
 
-const s = new Set([1,2,3,4])
-s.add(5)
 
-console.log(s)
+////////////////////////////////
+
+
+//// Child Sets, extends, super
+
+
+// class Set2 extends Set{
+//   constructor(){
+//     super(arr)
+//     this.original = arr
+//   }
+// }
+//
+// const s2 = new Set2
+//
+// for (i=0;i<=arr.length; i++){
+//   console.log(s2.arr[i])
+//   console.log("<3")
+//
+// }
+
+/////////////////////////////////
+
+
+// const elems = ['a','b','c']
+//
+// function Tele(){
+//   return (
+//     <Tele>
+//     <Ele q={elems[0]} />
+//     <Ele q={elems[1]} />
+//     <Ele q={elems[2]} />
+//     </Tele>
+//   )
+// }
+//
+// Tele()
